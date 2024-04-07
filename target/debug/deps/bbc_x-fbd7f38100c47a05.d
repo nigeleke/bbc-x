8192270779b0cf85545a6d59a3f307ac462608f2
Nigel Eke <nigeleke@users.noreply.github.com>
2024-04-07 01:35:57 +0000

    Release bbc-x@0.1.12

---
+++ target/debug/deps/bbc_x-fbd7f38100c47a05.d
@@ -0,0 +1,18 @@
+/home/runner/work/bbc-x/bbc-x/target/debug/deps/bbc_x-fbd7f38100c47a05: src/main.rs src/args.rs src/bbc3/mod.rs src/bbc3/assembler.rs src/bbc3/assembly.rs src/bbc3/ast.rs src/bbc3/grammar.rs src/bbc3/parser.rs src/bbcx/mod.rs src/core.rs src/language.rs src/list_writer.rs src/model.rs src/result.rs
+
+/home/runner/work/bbc-x/bbc-x/target/debug/deps/bbc_x-fbd7f38100c47a05.d: src/main.rs src/args.rs src/bbc3/mod.rs src/bbc3/assembler.rs src/bbc3/assembly.rs src/bbc3/ast.rs src/bbc3/grammar.rs src/bbc3/parser.rs src/bbcx/mod.rs src/core.rs src/language.rs src/list_writer.rs src/model.rs src/result.rs
+
+src/main.rs:
+src/args.rs:
+src/bbc3/mod.rs:
+src/bbc3/assembler.rs:
+src/bbc3/assembly.rs:
+src/bbc3/ast.rs:
+src/bbc3/grammar.rs:
+src/bbc3/parser.rs:
+src/bbcx/mod.rs:
+src/core.rs:
+src/language.rs:
+src/list_writer.rs:
+src/model.rs:
+src/result.rs:

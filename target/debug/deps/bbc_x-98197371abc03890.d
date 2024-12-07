8f27be87e3d36ed83b42331554c99053358bcb60
Nigel Eke <nigeleke@users.noreply.github.com>
2024-12-07 05:38:40 +0000

    Release bbc-x@0.1.20

---
+++ target/debug/deps/bbc_x-98197371abc03890.d
@@ -0,0 +1,32 @@
+/home/runner/work/bbc-x/bbc-x/target/debug/deps/bbc_x-98197371abc03890: src/main.rs src/args.rs src/bbc3/mod.rs src/bbc3/assembler.rs src/bbc3/assembly.rs src/bbc3/ast.rs src/bbc3/grammar.rs src/bbc3/parser.rs src/bbcx/mod.rs src/bbcx/assembler.rs src/bbcx/assembly.rs src/bbcx/ast.rs src/bbcx/charset.rs src/bbcx/executor.rs src/bbcx/grammar.rs src/bbcx/memory/mod.rs src/bbcx/memory/convert/mod.rs src/bbcx/memory/instruction.rs src/bbcx/memory/result.rs src/bbcx/memory/state.rs src/bbcx/memory/word.rs src/bbcx/parser.rs src/bbcx/result.rs src/core.rs src/language.rs src/list_writer.rs src/model.rs src/result.rs
+
+/home/runner/work/bbc-x/bbc-x/target/debug/deps/bbc_x-98197371abc03890.d: src/main.rs src/args.rs src/bbc3/mod.rs src/bbc3/assembler.rs src/bbc3/assembly.rs src/bbc3/ast.rs src/bbc3/grammar.rs src/bbc3/parser.rs src/bbcx/mod.rs src/bbcx/assembler.rs src/bbcx/assembly.rs src/bbcx/ast.rs src/bbcx/charset.rs src/bbcx/executor.rs src/bbcx/grammar.rs src/bbcx/memory/mod.rs src/bbcx/memory/convert/mod.rs src/bbcx/memory/instruction.rs src/bbcx/memory/result.rs src/bbcx/memory/state.rs src/bbcx/memory/word.rs src/bbcx/parser.rs src/bbcx/result.rs src/core.rs src/language.rs src/list_writer.rs src/model.rs src/result.rs
+
+src/main.rs:
+src/args.rs:
+src/bbc3/mod.rs:
+src/bbc3/assembler.rs:
+src/bbc3/assembly.rs:
+src/bbc3/ast.rs:
+src/bbc3/grammar.rs:
+src/bbc3/parser.rs:
+src/bbcx/mod.rs:
+src/bbcx/assembler.rs:
+src/bbcx/assembly.rs:
+src/bbcx/ast.rs:
+src/bbcx/charset.rs:
+src/bbcx/executor.rs:
+src/bbcx/grammar.rs:
+src/bbcx/memory/mod.rs:
+src/bbcx/memory/convert/mod.rs:
+src/bbcx/memory/instruction.rs:
+src/bbcx/memory/result.rs:
+src/bbcx/memory/state.rs:
+src/bbcx/memory/word.rs:
+src/bbcx/parser.rs:
+src/bbcx/result.rs:
+src/core.rs:
+src/language.rs:
+src/list_writer.rs:
+src/model.rs:
+src/result.rs:
